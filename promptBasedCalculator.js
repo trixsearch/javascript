692815b84c6a12a5cf54a1a803e09f95ce51679d
Abhinav Sahu <70811457+trixsearch@users.noreply.github.com>
2024-02-22 12:27:41 +0530

    added comments

--- promptBasedCalculator.js
+++ promptBasedCalculator.js
@@ -1,21 +1,30 @@
+//by @trixsearch
+// always free to work and contribute
+//@trixsearch at github and many more sites
+
+
 var num;
 var result = 0;
 var m= true ;
 
+
+// idhar hum ek choice dene wale hai ki aapko aage add karna hai ya nhi
+
 function check(choice){
     if (choice=="y") {
+        //agar add karna hoga toh ye kar dena
 
         m = true;
         num= parseInt(prompt("give a number: "));
         result = result + num;
-        
+        //result mein humne bus purane number ko sath mein addd kar diya hai
         
     } else {
         m = false;
         
     }
 }
-
+// ye loop sey humne decision le liya hai ki jabtak humko m ki value false nhi milti tab tak humko karte rahna hai 
 
 do{
     // result = result + num;
